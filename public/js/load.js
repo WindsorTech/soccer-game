479a5cacde5801392cacfc7d8a905a434318dd8a
windsor80 <windsorjunior@hotmail.com>
2017-12-08 22:11:07 -0500

    updates

--- public/js/load.js
+++ public/js/load.js
@@ -3,13 +3,6 @@ var loadState = {
 	preload: function() {
 
 		var loadingLabel = game.add.text(80, 150, 'LOADING...', {font: '30px Arial', fill: '#ffffff'});
-        // Mobile resposive
-        game.scale.scaleMode = Phaser.ScaleManager.RESIZE ;
-        game.scale.pageAlignHorizontally = true;
-        game.scale.pageAlignVertically = true;
-        game.canvas.style.width = '100%';
-        game.canvas.style.height = '100%';
-        game.scale.refresh();
 
 		//Load GameImages
     	game.load.image('futebol', '../images/football-field.jpg');
